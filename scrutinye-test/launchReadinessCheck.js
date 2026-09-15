a9baaf942f250623f0b3896c2b0c2096fd7f3e95
dineshmagizh93 <dineshmagizh93@gmail.com>
2026-09-15 11:31:17 +0530

    Retry with checkpoint tracing deployed

--- scrutinye-test/launchReadinessCheck.js
+++ scrutinye-test/launchReadinessCheck.js
@@ -14,3 +14,5 @@ module.exports = { getFirstOrDefault };
 // retry after LLM call timeout fix (391d25b)
 
 // retry after disabling HTTP keep-alive on LLM clients (ee8b856)
+
+// retry with checkpoint tracing deployed (a78c858)
